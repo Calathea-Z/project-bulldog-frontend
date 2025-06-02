626f385cb48428c7aeea7b7260ff6050f92f9f35
Zach Sykes <zsykes21@gmail.com>
2025-06-02 14:17:15 -0600

    fix linting error

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { getAccessToken, setAccessToken, api } from '@/services/api';
+import { getAccessToken, setAccessToken } from '@/services/api';
 import { isIOS } from '@/utils/device';
 
 /**
